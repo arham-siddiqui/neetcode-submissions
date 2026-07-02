9d4242a37118a96d76c5e2319fc7b2755d97341c
Arham Siddiqui <66856134+arham-siddiqui@users.noreply.github.com>
2026-07-01 20:13:23 -0700

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        openStack = []
+        closedStack = []
+        counterPart = {
+            '(' : ')',
+            '{' : '}',
+            '[' : ']',
+            ')' : '(',
+            '}' : '{',
+            ']' : '['
+        }
+
+        for p in s:
+
+            if p == '(' or p == '{' or p == '[':
+                openStack.append(p)
+                closedStack.append(counterPart[p])
+            
+            if p == ')' or p == '}' or p == ']':
+                if len(closedStack) == 0:
+                    return False
+                if p == closedStack[-1]:
+                    del closedStack[-1]
+                    del openStack[-1]
+                else:
+                    return False
+        if len(openStack) == 0 and len(closedStack) == 0:
+            return True        
+        return False
